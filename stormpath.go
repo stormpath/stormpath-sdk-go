a34ed119a161b87fe2fb338c426416f2271eda5e
Randall Degges <r@rdegges.com>
2014-04-27 17:54:33 -0700

    Cleaning up.

--- stormpath.go
+++ stormpath.go
@@ -1,32 +1,20 @@
 package stormpath
 
-import (
-	"encoding/json"
-	"errors"
-	"io"
-	"net/http"
-)
+import ()
 
 const (
-	VERSION    = "0.0.1"
-	USER_AGENT = "go-stormpath/" + VERSION
-	BASE_URL   = "https://api.stormpath.com/v1"
-	ENABLED    = "enabled"
-	DISABLED   = "disabled"
+	VERSION          = "0.0.1"
+	USER_AGENT       = "go-stormpath/" + VERSION
+	BASE_URL         = "https://api.stormpath.com/v1"
+	ENABLED          = "enabled"
+	DISABLED         = "disabled"
+	PAGINATION_LIMIT = 100
 )
 
 // The ApiKeyPair object is meant for storing Stormpath credentials.
 type ApiKeyPair struct {
 	Id     string
 	Secret string
-}
-
-// The Client object allows you to easily communicate with the Stormpath API
-// service.
-type Client struct {
-	Keypair   *ApiKeyPair
-	Transport *http.Transport
-	Tenant    *Tenant
 }
 
 // A Tenant is a globally unique namespace.
@@ -43,6 +31,14 @@ type Application struct {
 	Description string  `json:"description"`
 	Status      string  `json:"status"`
 	Tenant      *Tenant `json:"-"`
+}
+
+// A list of Stormpath Applications.
+type ApplicationList struct {
+	Href   string         `json:"href"`
+	Offset int            `json:"offset"`
+	Limit  int            `json:"limit"`
+	Items  *[]Application `json:"items"`
 }
 
 // A generic Stormpath collection -- this is used internally to handle
@@ -107,88 +103,4 @@ type CustomData struct {
 	Href       string
 	CreatedAt  string
 	ModifiedAt string
-}
-
-// NewClient generates a new Stormpath Client, given an ApiKeyPair as input.
-// It will then use the ApiKeyPair to attempt to fetch the current Stormpath
-// Tenant.  Returns an initialized Client (thread safe) and any error
-// encountered.
-func NewClient(keypair *ApiKeyPair) (*Client, error) {
-	client := &Client{
-		Transport: &http.Transport{},
-		Keypair:   keypair,
-	}
-
-	tenant, err := client.GetTenant()
-	if err != nil {
-		return nil, err
-	}
-	client.Tenant = tenant
-
-	return client, nil
-}
-
-// Request makes an HTTP request to the Stormpath API, using the user's
-// credentials automatically.  Returns an HTTP response and any error
-// encountered.
-func (client *Client) Request(method string, url string, body io.Reader) (*http.Response, error) {
-	req, _ := http.NewRequest(method, url, body)
-	req.Header.Set("User-Agent", USER_AGENT)
-	req.SetBasicAuth(client.Keypair.Id, client.Keypair.Secret)
-
-	resp, err := client.Transport.RoundTrip(req)
-	if err != nil {
-		return nil, err
-	}
-	return resp, nil
-}
-
-// GetTenant returns the current Tenant and any error encountered.
-func (client *Client) GetTenant() (*Tenant, error) {
-	resp, err := client.Request("GET", BASE_URL+"/tenants/current", nil)
-	if err != nil {
-		return nil, err
-	}
-	resp.Body.Close()
-
-	if resp.StatusCode != 302 {
-		return nil, errors.New("Didn't receive HTTP 302 when requesting tenant.")
-	} else if resp.Header.Get("Location") == "" {
-		return nil, errors.New("No Location header found when requesting tenant.")
-	}
-
-	resp, err = client.Request("GET", resp.Header.Get("Location"), nil)
-	if err != nil {
-		return nil, errors.New("Got HTTP error when requesting tenant.")
-	}
-	defer resp.Body.Close()
-
-	tenant := &Tenant{}
-	dec := json.NewDecoder(resp.Body)
-	err = dec.Decode(tenant)
-	if err != nil {
-		return nil, err
-	}
-
-	return tenant, nil
-}
-
-// GetApplications returns an ApplicationCollection, and any error encountered.
-// TODO: Iterate over *all* applications, not just some.
-func (client *Client) GetApplications() (*ApplicationCollection, error) {
-	resp, err := client.Request("GET", client.Tenant.Href+"/applications", nil)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	//collection := &Collection{}
-	ac := &ApplicationCollection{}
-	dec := json.NewDecoder(resp.Body)
-	err = dec.Decode(ac)
-	if err != nil {
-		return nil, err
-	}
-
-	return ac, nil
 }
